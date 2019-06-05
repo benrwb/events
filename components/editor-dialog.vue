4dadfb70e02b9f35c232fa5fd636cb9ec36a6239
Ben Blewitt <benrwb@googlemail.com>
2019-06-05 17:35:43 +0100

    Markdown logo

--- components/editor-dialog.vue
+++ components/editor-dialog.vue
@@ -14,7 +14,7 @@
                                 <span v-if="!!dbitem.notes">Notes</span>
                             </bootstrap-nav> 
                             <bootstrap-nav v-show="!!dbitem.notes"
-                                           value="markdown" v-model="activeTab">MD</bootstrap-nav>
+                                           value="markdown" v-model="activeTab">M🠋</bootstrap-nav>
                         </ul>
                     </h4>
                 </div>
