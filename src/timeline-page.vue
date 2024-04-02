3465af501c4398d1e487a7f0212894798b747128
Ben Blewitt <benrwb@googlemail.com>
2024-04-02 17:30:51 +0100

    change link icon

--- src/timeline-page.vue
+++ src/timeline-page.vue
@@ -63,7 +63,7 @@
                             v-bind:href="item.link"
                             class="emoji"
                             style="text-decoration: none"
-                            ><span class="glyphicon glyphicon-new-window"
+                            ><span class="glyphicon glyphicon-link"
                                                     style="padding: 0 3px"></span></a>
                         </div>
                         <div v-show="!isCollapsed(item)">
